160b09396281f91b9ae3601db7f85444507331d9
jnoweb <i.am@jnoweb.com>
2020-04-17 12:10:58 +0200

    Fixing depracation on backend

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const BASE_URL = "https://corona.lmao.ninja";
 // ! Fetch covid-19 data by country
 export async function fetchDataByCountry(country){
     // ! Build url with the string template literal
-    const url = `${BASE_URL}/countries/${country}`;
+    const url = `${BASE_URL}/v2/countries/${country}`;
     // ! Make a get request to the server to fetch covid-19 data for a country
     const response = await fetch(url);
     // ! Get the data from the server reponse
